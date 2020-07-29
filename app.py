2be16ba681cf87929e228854b6412dd9fd9103a1
Shubham Kumar <sshubhamk1@hotmail.com>
2020-07-29 20:49:04 +0530

    app.py updated

--- app.py
+++ app.py
@@ -8,12 +8,16 @@ def index():
     return("<b>Welcome, send post request <a href='/login'>here</a> to check api")
 @app.route("/login", methods=["POST"])
 def login():
+    if not (request.form['email'] and request.form['password']):
+        return("<b>pass email and password</b>")
     auth = jwt.encode({"auth":request.form['email'],"email":request.form['password']},key, algorithm="HS256")
     value = auth.decode('utf-8')
     return("Authentication code is <p><b>{}</b></p>".format(value))
 
 @app.route('/verify')
 def verify():
+    if not request.args.get('code'):
+        return("<b> pass token as code</b>")
     auth = jwt.decode(request.args.get('code'),key)
     return(auth)
 
