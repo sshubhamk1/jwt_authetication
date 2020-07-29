b483e37c404c38896d2994f9ddb06020dbf1968e
Shubham Kumar <sshubhamk1@hotmail.com>
2020-07-29 20:42:58 +0530

    jwt done

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,request
+import jwt
+
+app = Flask(__name__)
+key ="this is a secret key for encryption"
+@app.route('/')
+def index():
+    return("<b>Welcome, send post request <a href='/login'>here</a> to check api")
+@app.route("/login", methods=["POST"])
+def login():
+    auth = jwt.encode({"auth":request.form['email'],"email":request.form['password']},key, algorithm="HS256")
+    value = auth.decode('utf-8')
+    return("Authentication code is <p><b>{}</b></p>".format(value))
+
+@app.route('/verify')
+def verify():
+    auth = jwt.decode(request.args.get('code'),key)
+    return(auth)
+
+
+if __name__ == "__main__":
+    app.run()
